d66ddf252122635b84164cfd45d5b4461a6d77a0
Yasseen-A <36662301+Yasseen-A@users.noreply.github.com>
2018-04-18 19:45:34 +0100

    hiding finally working

--- javascripts/javascript.js
+++ javascripts/javascript.js
@@ -1,18 +1,17 @@
-
-  function EatingTarget() {
-    var calories = prompt("How many calories would you like to consume?");
-    if (calories != null) {
-      document.getElementById("CalorieCounter").innerHTML =
-        calories + "<span class='countertext'>calories</span>";
-      document.getElementById('CalorieButton').style.display = 'none';
-    }
+function EatingTarget() {
+  var calories = prompt("How many calories would you like to consume?");
+  if (calories != null) {
+    document.getElementById("CalorieCounter").innerHTML =
+      calories + "<span class='countertext'>calories</span>";
+    document.getElementById('CalorieButton').style.display = 'none';
   }
+}
 
-  function MyBurningTarget() {
-    var calories_burnt = prompt("How many calories would you like to burn?");
-    if (calories_burnt != null) {
-      document.getElementById("ExerciseCounter").innerHTML =
-        calories_burnt + "<span class='countertext'>calories</span>";
-      document.getElementById('ExerciseButton').style.display = 'none';
-    }
+function MyBurningTarget() {
+  var calories_burnt = prompt("How many calories would you like to burn?");
+  if (calories_burnt != null) {
+    document.getElementById("ExerciseCounter").innerHTML =
+      calories_burnt + "<span class='countertext'>calories</span>";
+    document.getElementById('ExerciseButton').style.display = 'none';
   }
+}
